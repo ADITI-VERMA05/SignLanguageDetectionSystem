3272f2d09eb3002b957e9d250219c02e517abe1f
ADITI_VERMA05 <aditiverma1605@gmail.com>
2025-04-09 07:11:26 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'signLanuages',
+    version = '0.0.0',
+    author = 'AditiVerma',
+    author_email = 'aditiverma1605@gmail.com',
+    packages = find_packages(),
+    install_requires =[]
+)
