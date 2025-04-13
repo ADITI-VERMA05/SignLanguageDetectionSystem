843909a6172587d20608b6d323bf32685648f466
ADITI_VERMA05 <aditiverma1605@gmail.com>
2025-04-13 19:47:26 +0530

    logger & exception added

---
+++ app.py
@@ -0,0 +1,11 @@
+import sys
+from signLanguage.logger import logging
+from signLanguage.exception import SignException
+
+#logging.info("Welcome to the Project")
+
+try:
+    a = 7/'9'
+    
+except Exception as e:
+    raise SignException(e, sys) from e
