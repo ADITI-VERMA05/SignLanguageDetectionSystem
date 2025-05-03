51a0c3ce0f99a787f513b42961876471e6b8b2ca
ADITI_VERMA05 <aditiverma1605@gmail.com>
2025-05-03 10:54:45 +0530

    Data ingestion added

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-import sys
-from signLanguage.logger import logging
-from signLanguage.exception import SignException
+# import sys
+# from signLanguage.logger import logging
+# from signLanguage.exception import SignException
 
-#logging.info("Welcome to the Project")
+# #logging.info("Welcome to the Project")
 
-try:
-    a = 7/'9'
+# try:
+#     a = 7/'9'
     
-except Exception as e:
-    raise SignException(e, sys) from e
+# except Exception as e:
+#     raise SignException(e, sys) from e
+
+from signLanguage.pipeline.training_pipeline import TrainPipeline
+
+obj = TrainPipeline()
+obj.run_pipeline()
